3a623c5d6780c023ba2a4f3cb77102ce41639e3a
vishnu mishra <vishnumishra8910@gmail.com>
2025-07-13 01:27:28 +0530

    Fix: added todoapp as a regular folder instead of submodule

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,32 +1,85 @@
-import { useState, useRef } from 'react';
-import './App.css';
-import { getAutoCompleteData } from './getAutoCompleteData';
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addToDoItem, deleteItem, updateItem } from "./store/todoListSlice";
+import { DeleteButton, EditButton } from "./components/UXComponents";
+import ListItem from "./components/ListItem";
 
 function App() {
-  const [respData, setRespData] = useState([]);
-  const inputElement = useRef();
-  const handleAutoCompleteSearch = async (event) => {
-    console.log(event.target.value);
-    const dataResp = await getAutoCompleteData(event.target.value);
-    setRespData(event.target.value && (dataResp || []));
+  // console.log(addToDoItem);
+  const [listInput, setListInput] = useState('');
+  const [listState, setListState] = useState([]);
+  const [listItemUpdate, setListItemUpdate] = useState('');
+  // const [updatingListItemId, setUpdatingListItemId] = useState('');
+  const dispatch = useDispatch();
+  // const storeState = useSelector((state)=> state);
+  // console.log('storeState is ', storeState);
+  const listItems = useSelector((state)=> state.todoList);
+
+  console.log('listItems is ', listItems);
+
+  useEffect(() => {
+    setListState(listItems);
+  },[listItems]);
+
+  const listInputHandler = (event) => {
+    setListInput(event.target.value);
+  }
+  const addListHandler = () => {
+    try{
+      const uniqueId = Math.floor(100000 + Math.random()*900000);
+      const listState = {id: uniqueId, value: listInput};
+      console.log(listState);
+      listInput.length>0 && dispatch(addToDoItem(listState));
+      setListInput('');
+    }catch(err){
+      console.log(err);  
+    }
   };
-  const optionsHandler = (event) => {
-    inputElement.current.value = event.target.innerText;
-    setRespData([]);
+
+  const keyEventHandler = (event) => {
+    if(event.key ==='Enter'){
+      !!listItemUpdate ? updateListHandler() : addListHandler();
+    }
+  }
+
+  const onEditHandler = (itemId) => {
+    console.log('EditHandler Called');
+    const ListItem = listState.find((item)=>item.id==itemId);
+    setListInput(ListItem.value);
+    setListItemUpdate(itemId);
+  };
+
+  const updateListHandler = () => {
+    console.log('updateListHandler called');
+    dispatch(updateItem({id: listItemUpdate, value: listInput}));
+    setListInput('');
+    setListItemUpdate('');
+  };
+
+  const onDeleteHandler = (ItemId) => {
+    console.log('DeleteHandler Called');
+    dispatch(deleteItem(ItemId))
   };
 
   return (
-    <div className="App">
-      <h2>Autocomplete Search Bar</h2>
-      <input type="text" id="searchbox" ref={inputElement} className='searchbox' onKeyUp={handleAutoCompleteSearch}/>
-      <br/>
-      { respData.length>0 && 
-        <div id="respdata" className='respdatadiv'>
-          {respData.map((item,index)=>{
-            return <><span key={`${item.name}_${index}`} className='suggesteditems' onClick={optionsHandler}>{item.name}</span><br/></>
+    <div className="appcontainer">
+      <div className="flex flex-col justify-center p-3 text-center m-2 mt-8 pb-8 rounded">
+        <p className="text-6xl w-12/12 flex justify-center p-8 rounded font-semibold shadow-lg">📝 To Do List 📝</p>
+        <div className="mt-16 mb-16 flex gap-0.5 justify-center overflow-auto">
+          <input value={listInput} type="text" className="w-4/12 border rounded mr-1 p-1" placeholder=" List Name " onChange={listInputHandler} onKeyDown={ keyEventHandler}/>
+          { !!!listItemUpdate && 
+            <button className="border font-semibold bg-green-600 font-stretch-50% text-white w-18 rounded cursor-pointer hover:scale-x-105" onClick={addListHandler}>Add</button>
+          }
+            { !!listItemUpdate && 
+            <button className="border font-semibold bg-green-600 font-stretch-50% text-white w-18 rounded cursor-pointer hover:scale-x-105" onClick={updateListHandler}>Update</button>
+          }
+          </div>
+        <div className="flex flex-col justify-center self-center text-center w-8/12 shadow-lg p-10 rounded ">
+          {listState.length>0 && listState.map((item)=>{
+            return <ListItem key={item.id} item={item.value}><EditButton onEditHandler={() => onEditHandler(item.id)}/><DeleteButton onDeleteHandler={() => onDeleteHandler(item.id)}/></ListItem>
           })}
         </div>
-      }
+      </div>
     </div>
   );
 }
