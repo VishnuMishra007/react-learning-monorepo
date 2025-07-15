3485918cbfc1152f1a6c3e00fc254427aa2b5e19
vishnu mishra <vishnumishra8910@gmail.com>
2025-07-15 21:44:13 +0530

    updated fix

--- quickfeast/src/App.jsx
+++ quickfeast/src/App.jsx
@@ -16,7 +16,10 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { routes } from './components/routes/routes';
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes, {
+    basename: "/quickfeast",
+  }
+);
 
 const App = () => {
   return <RouterProvider fallbackElement={<div>Loading...</div>} router={router} />;
