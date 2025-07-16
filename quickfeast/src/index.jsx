4e03f4c7482221632ee15a52564115a974e81f73
vishnu mishra <vishnumishra8910@gmail.com>
2025-07-16 11:29:27 +0530

    check

--- quickfeast/src/index.jsx
+++ quickfeast/src/index.jsx
@@ -4,4 +4,5 @@ import reduxstore from "./store/reduxstore";
 const { createRoot } = require("react-dom/client");
 
 const rootElement = createRoot(document.getElementById('root'));
-rootElement.render(<Provider store={reduxstore}><App/></Provider>);
+rootElement.render(<Provider store={reduxstore}> <h1 style={{ color: "green" }}>QuickFeast is Loading ✅</h1>
+</Provider>);
